a0582e81449e397a7199c39704b9f85a926e17f0
DevFaith <waitherafaith@outlook.com>
2025-12-20 22:45:01 +0300

    refactor routing: comment out Companies route and fix import path for Contact component

--- faith-portfolio/src/App.jsx
+++ faith-portfolio/src/App.jsx
@@ -4,8 +4,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Experience from "./pages/Experience";
 import Testimonials from "./pages/Testimonials";
-import Companies from "./pages/Companies";
-import Contact from "./pages/Contact";
+// import Companies from "./pages/Companies";
+import Contact from "./Pages/Contacts";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/companies" element={<Companies />} />
+        {/* <Route path="/companies" element={<Companies />} /> */}
         <Route path="/contact" element={<Contact />} />
       </Route>
     </Routes>
